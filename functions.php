d54b44678a49574b90503bdac998acbd065a8941
Stelly <emailisfun@stel.ly>
2024-04-29 16:17:15 -0500

    Update functions.php
    Added a function to get the current year and display it using a shortcode. This is mainly used for the copyright notice in site footers.

--- functions.php
+++ functions.php
@@ -6,4 +6,17 @@ function divi_child_enqueue_styles() {
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
 add_action('wp_enqueue_scripts', 'divi_child_enqueue_styles');
+
+// Define a function to get the current year
+function get_current_year() {
+    return date('Y');
+}
+
+// Register the shortcode
+add_shortcode('current_year', 'display_current_year');
+
+// Define the function to display the current year
+function display_current_year() {
+    return get_current_year();
+}
 ?>
